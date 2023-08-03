5256ca85889a093a8cf4113184b1a3761adb7eb5
saumya1411 <96434989+saumya1411@users.noreply.github.com>
2023-08-03 23:23:45 +0530

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import {AiFillMessage} from "react-icons/Ai"
+import {AiFillMessage} from "react-icons/ai"
 import Style from './Button.module.css'
 const Button = (props) => {
   
